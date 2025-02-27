10b732f10d1e95a37ee7024e9ed0cc3f4a86818e
delchev <delchevn@gmail.com>
2025-02-27 17:14:39 +0200

    new-ui - initial

--- application/src/test/java/com/codbex/hyperion/ui/IntegrationTest.java
+++ application/src/test/java/com/codbex/hyperion/ui/IntegrationTest.java
@@ -11,14 +11,11 @@
 package com.codbex.hyperion.ui;
 
 import com.codbex.hyperion.HyperionApplication;
-import com.codbex.hyperion.ui.tests.config.TestConfigurations;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.annotation.DirtiesContext;
 
-@Import(TestConfigurations.class)
 @AutoConfigureMockMvc
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = HyperionApplication.class)
