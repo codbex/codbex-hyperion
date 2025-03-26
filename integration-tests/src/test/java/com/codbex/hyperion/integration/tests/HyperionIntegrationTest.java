9f898d5b499b8834290687f742a666ec178f2ef3
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-03-26 08:57:15 +0200

    Use integration tests from dirigible (#252)

--- integration-tests/src/test/java/com/codbex/hyperion/integration/tests/HyperionIntegrationTest.java
+++ integration-tests/src/test/java/com/codbex/hyperion/integration/tests/HyperionIntegrationTest.java
@@ -8,7 +8,7 @@
  * SPDX-FileCopyrightText: 2022 codbex or an codbex affiliate company and contributors
  * SPDX-License-Identifier: EPL-2.0
  */
-package com.codbex.hyperion.ui.tests;
+package com.codbex.hyperion.integration.tests;
 
 import org.eclipse.dirigible.tests.UserInterfaceIntegrationTest;
 import org.springframework.context.annotation.Import;
