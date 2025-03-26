dc4f39ffd35464460b44cb6a46842d3cabe1d14b
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-03-26 11:59:46 +0200

    cleanup

--- integration-tests/src/test/java/com/codbex/hyperion/integration/tests/common/HyperionCmsSuiteIT.java
+++
@@ -1,6 +0,0 @@
-package com.codbex.hyperion.integration.tests.common;
-
-import org.eclipse.dirigible.integration.tests.api.javascript.cms.CmsSuiteIT;
-
-class HyperionCmsSuiteIT extends CmsSuiteIT {
-}
