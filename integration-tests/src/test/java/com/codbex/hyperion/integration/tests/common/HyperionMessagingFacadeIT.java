ff16b64b4eaedd9496e13df398057e5c6b2e35bd
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-03-26 15:41:24 +0200

    fixes

--- integration-tests/src/test/java/com/codbex/hyperion/integration/tests/common/HyperionMessagingFacadeIT.java
+++
@@ -1,6 +0,0 @@
-package com.codbex.hyperion.integration.tests.common;
-
-import org.eclipse.dirigible.integration.tests.api.java.messaging.MessagingFacadeIT;
-
-class HyperionMessagingFacadeIT extends MessagingFacadeIT {
-}
