a0f26638c60c690e56dc3eba470fcb23cad0aa3f
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-03-31 15:18:36 +0300

    remove classes

--- integration-tests/src/test/java/com/codbex/hyperion/integration/tests/common/HyperionCreateNewProjectIT.java
+++
@@ -1,6 +0,0 @@
-package com.codbex.hyperion.integration.tests.common;
-
-import org.eclipse.dirigible.integration.tests.ui.tests.CreateNewProjectIT;
-
-class HyperionCreateNewProjectIT extends CreateNewProjectIT {
-}
--- integration-tests/src/test/java/com/codbex/hyperion/integration/tests/common/HyperionCsvimIT.java
+++
@@ -1,6 +0,0 @@
-package com.codbex.hyperion.integration.tests.common;
-
-import org.eclipse.dirigible.integration.tests.ui.tests.CsvimIT;
-
-class HyperionCsvimIT extends CsvimIT {
-}
